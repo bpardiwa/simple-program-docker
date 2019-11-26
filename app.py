46b0ac4f8d56d203027a7452655e141130a083d4
bpardiwa <bomi.pardiwalla@dhl.com>
2019-11-26 21:22:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import os
 from flask import Flask
-app= Flask(__name__)
 
-@app.route("/")
-def main():
-	return "Welcome!"
+app = Flask(__name__)
 
-app.route('/howareyou')
-
+@app.route('/')
 def hello():
-	return 'I am good, how about you?'
+    return 'Hello World!'
+def Fun():
+    return 'Having Fun!'
 
-if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
